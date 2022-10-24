d94bfc7b26547ba203355bba7f2b66081419ef18
Andrea Amaya <andreaa.malin@gmail.com>
2022-10-23 21:11:35 -0600

    Grow and harvest each crop

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from random import random, randint
 from seasons import *
 
 season = Season()
-crop = []
-available_crops = []
+full_crop = []
 initialMoney = 500
 # Next day that will rain
 def getArrivalTime(p, _lambda):
@@ -14,18 +13,28 @@ def getArrivalTime(p, _lambda):
 _rain_lambda = 1
 next_rain_day = getArrivalTime(random(), _rain_lambda)
 
+# Season initial crop
 season.currentSeason()
-available_crops = season.available_crops
-
-available_costs = [item().cost for item in available_crops]
+available_costs = [item().cost for item in season.available_crops]
 
 while initialMoney:
-  randomCrop = available_crops[randint(0, len(available_crops) - 1)]
+  randomCrop = season.available_crops[randint(0, len(season.available_crops) - 1)]
   if (randomCrop().cost <= initialMoney):
     initialMoney -= randomCrop().cost
-    crop.append(randomCrop)
+    full_crop.append(randomCrop())
   
   if initialMoney < min(available_costs):
     break
 
-print(crop)
+DAYS_TO_SIMULATE = 30
+
+print('INITIAL MONEY', initialMoney)
+
+for i in range(DAYS_TO_SIMULATE):
+  for crop in full_crop:
+    crop.grow()
+    if (crop.is_ready):
+      initialMoney += crop.harvest()
+      full_crop.remove(crop)
+
+print('FINAL MONEY', initialMoney)
